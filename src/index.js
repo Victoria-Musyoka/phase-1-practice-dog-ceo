e60b86417b1f4e3c9750429cebf5a806ea1ec9c2
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-06-18 21:02:40 +0300

    tried assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+fetch(imgUrl)
+  .then(resp => resp.json())
+  .then(json => console.log(images))
+
+
+for (const item of images.message) {
+const pictures= document.querySelector('#dog-image-container');
+ pictures.innerHTML += `<img src="${item}" alt="">`
+}
+
+
+
+
+const breedUrl = "https://dog.ceo/api/breeds/list/all"
+fetch(breedUrl)
+.then(resp=>resp.json())
+.then(json=>renderBreed(json))
+
+for(const item in breeds.message){
+    let list=document.querySelector("#dog-breeds")
+    const li=document.createElement("li")
+    li.innerHTML=itemlist.appendChild(li);
+    li.addEventListener('click', ()=>{
+        list.getElementsByClassName.color="red"
+    })
+}
